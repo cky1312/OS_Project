f8c9c4abc616499a822e5a1db2f00b5f2347ba81
Chandresh Yadav <48850444+cky1312@users.noreply.github.com>
2020-03-30 19:05:25 +0530

    Update OS_Project.cpp

--- OS_Project.cpp
+++ OS_Project.cpp
@@ -36,7 +36,7 @@ void check(int a,int b,int c)
 	
 	if(b > ingredientLimit)
 	{
-		cout<<"Overflow, The kitchen cannot have this much ingredients,\n";
+		cout<<"Overflow, The kitchen cannot have this much ingredients.\n";
 		details();
 	}
 	
